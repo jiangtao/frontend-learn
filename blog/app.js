65a77d0f74fb02975e8db0c2ba816196f52bb332
huangjiangtao <hjt@megvii.com>
2015-03-29 15:36:24 +0800

    取巧实现express 渲染.html

--- blog/app.js
+++ blog/app.js
@@ -16,7 +16,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -24,7 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 app.use('/users', users);
 app.use('/article', article);
--- blog/routes/users.js
+++ blog/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
-
-/* GET users listing. */
+var path = require('path');
+var users = {
+  title: '用户列表'
+};
+var views = {
+  dir: path.normalize(path.join(__dirname, '../views/users'))
+};
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users/index', { title: users.title });
+});
+// 使用html作为模版 用来测试调试使用
+router.get('/file/:name', function(req, res, next) {
+  var name = req.params.name;
+  res.sendFile(path.join(views.dir, name));
 });
 
 module.exports = router;
